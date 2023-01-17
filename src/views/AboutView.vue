488c4b1c0746a5b24ca2a45e96861b30a208f03e
Anthony Wong <nakagame02@gmail.com>
2023-01-17 23:50:14 +0900

    Initial commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about lg:items-center lg:flex lg:min-h-screen">
+    <h1>This is an about page</h1>
+    <div>123</div>
+    <div>456</div>
+  </div>
+</template>
+
+<style>
+/* @media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+} */
+</style>
