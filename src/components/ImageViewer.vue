7fa9c8adf6939f7b2bd982d26c71e6857d22b9fc
Anthony Wong <nakagame02@gmail.com>
2023-01-28 01:08:39 +0900

    feat: add open modal effect

--- src/components/ImageViewer.vue
+++ src/components/ImageViewer.vue
@@ -1,8 +1,23 @@
 <script setup lang="ts">
 import type { ModalDetail } from "@/types"
 import { generateURL } from "@/utils/commonUtils"
-import { computed } from "vue"
+import { computed, onMounted, ref } from "vue"
 import IconButton from "./IconButton.vue"
+
+const fields = [
+  {
+    label: "name",
+    key: "name",
+  },
+  {
+    label: "size",
+    key: "size",
+  },
+  {
+    label: "last modified",
+    key: "lastModified",
+  },
+]
 
 interface Props {
   detail: ModalDetail
@@ -11,20 +26,13 @@ interface Props {
 
 const props = defineProps<Props>()
 
-const fields = [
-    {
-        label: 'name',
-        key: 'name'
-    },
-    {
-        label: 'size',
-        key: 'size'
-    },
-    {
-        label: 'last modified',
-        key: 'lastModified'
-    },
-]
+const isModalReady = ref<boolean>(false)
+
+onMounted(() => {
+  setTimeout(() => {
+    isModalReady.value = true
+  }, 150)
+})
 
 const withUrl = computed(() => {
   return {
@@ -32,10 +40,22 @@ const withUrl = computed(() => {
     file: props.detail.content.file,
   }
 })
+
+const handleClose = () => {
+//   isModalReady.value = false
+//   setTimeout(() => {
+    props.onCloseClick()
+//   }, 150)
+}
 </script>
 
 <template>
-  <div class="w-full h-full flex flex-col text-slate-300">
+  <div
+    class="w-full h-full flex flex-col text-slate-300 transition-transform"
+    :class="{
+      'scale-0': !isModalReady,
+    }"
+  >
     <div
       class="w-100 flex mx-6 py-4 items-center justify-between border-b border-neutral-600 mb-4"
     >
@@ -46,7 +66,7 @@ const withUrl = computed(() => {
         name="close"
         class="cursor-pointer"
         icon-class-name="w-10 h-10 text-white"
-        @click="props.onCloseClick"
+        @click="handleClose"
       />
     </div>
     <div class="flex-1 flex justify-center overflow-hidden">
@@ -56,12 +76,14 @@ const withUrl = computed(() => {
     </div>
     <div class="flex justify-center py-6">
       <div class="w-full px-6">
-
-        <div v-for="(field, index) in fields" :key="index" class="flex flex-row  border-b border-neutral-600 py-2">
+        <div
+          v-for="(field, index) in fields"
+          :key="index"
+          class="flex flex-row border-b border-neutral-600 py-2"
+        >
           <div class="capitalize basis-2/5">{{ field.label }} :</div>
           <div class="basis-3/5">{{ withUrl.file[field.key] }}</div>
         </div>
-
       </div>
     </div>
   </div>
